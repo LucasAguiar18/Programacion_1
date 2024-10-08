f30f178de31136d313943a2d23fa71d63b0abb3d
LucasAguiar18 <holalucas17.arroyo@gmail.com>
2024-10-08 11:52:51 -0300

    commit

---
+++ temas24des3.py
@@ -0,0 +1,14 @@
+# Temperatura en celsius
+
+temp_celsius = 50
+
+#Formula Fahrenheit
+temp_Fahrenheit = (temp_celsius * 9 / 5) + 32
+
+#Formula Kelvin
+temp_kelvin = temp_celsius + 273.15
+
+# Mostrar resultados
+print(f"Temperatura en Celsius: {temp_celsius}°C")
+print(f"Temperatura en Fahrenheit: {temp_Fahrenheit}°F")
+print(f"Temperatura en Kelvin: {temp_kelvin}K")
